a6cfe24563c00fe1e789ca93fd5a696f9348927d
vigneshwaran M <vikidotcom95@gmail.com>
2020-03-08 10:42:42 +0530

    func_pointer.c

---
+++ func_pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Add(int a,int b)
+{
+	return a+b;
+}
+int main()
+{
+	int c;
+	int (*p)(int,int);
+	p = Add;
+	c = (*p)(2,3);
+	printf("%d\n",c);
+	return 0;
+}
